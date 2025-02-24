8bbabe6c794d5fc7041799d6230f1706062e41a0
Bronny <bronnyhamilton@gmail.com>
2025-02-24 12:18:16 +1100

    feat: fixed port

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 // Import the configured server 
 // and run it 
 
-require("dotenv").config();
+const dotenv = require("dotenv");
+dotenv.config();
 
 const { app } = require("./server");
 
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
 	console.log("Server is running on port " + PORT);
